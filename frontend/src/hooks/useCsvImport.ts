21680acac670d5ca3c127bc2c727c15fd38e18b5
Pavan Kumar <pavankmr1902@gmail.com>
2026-07-12 17:01:04 +0530

    added deployed backend url

--- frontend/src/hooks/useCsvImport.ts
+++ frontend/src/hooks/useCsvImport.ts
@@ -40,7 +40,7 @@ export const useCsvImport = () => {
 
     try {
       // Assuming your backend is running on port 3001
-      const response = await fetch('http://localhost:3001/api/upload', {
+      const response = await fetch('https://csv-importer-84qa.onrender.com/api/upload', {
         method: 'POST',
         body: formData,
       });
@@ -48,8 +48,6 @@ export const useCsvImport = () => {
       if (!response.ok) throw new Error('API processing failed.');
 
       const data = await response.json();
-
-      console.log('API response status:', data);
       setResults(data);
       setStatus('success');
     } catch (err: any) {
